50702d0240c7c04db732dc097ce60e4f4d799cc8
Naqeeb Ahmed <naqeebahmedsahi@gmail.com>
2025-02-12 10:15:01 +0500

    Revert "New XHange"
    This reverts commit 81c2390bbc83157d40bcd4f4335ae3f368ba68ec.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import './style.css';
+import './style.css'
+
 
 document.querySelector("#app").innerHTML = `
   <div class="container">
     <h1>Hey 💖</h1>
     
+    <button id="showPartner">Do you want to know your partner?</button>
+    
+    <h2 id="partnerName" class="hidden">First Name 💘</h2>
+
+    <p id="loveText" class="hidden">First Name ❤️ Second name</p>
   </div>
 `;
 
 document.querySelector("#showPartner").addEventListener("click", () => {
   document.querySelector("#partnerName").classList.remove("hidden");
   document.querySelector("#loveText").classList.remove("hidden");
-  document.querySelector("#questionSection").classList.remove("hidden");
 });
 
-document.querySelector("#yesBtn").addEventListener("click", () => {
-  document.querySelector("#finalMessage").innerText = "You are deeply in love with Cheeko ❤️";
-  document.querySelector("#finalMessage").classList.remove("hidden");
-});
 
-document.querySelector("#noBtn").addEventListener("click", () => {
-  document.querySelector("#finalMessage").innerText = "I know in your heart you don’t want to touch No, so click Yes...";
-  document.querySelector("#finalMessage").classList.remove("hidden");
-});
